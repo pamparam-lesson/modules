674085261313c72a2193af2b1f32f509c4f19f7a
pamparam <pamparam83@gmail.com>
2016-12-13 23:20:09 +0300

    add page change password profile

---
+++ modules/user/views/profile/index.php
@@ -0,0 +1,23 @@
+<?php
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+/* @var $this yii\web\View */
+/* @var $model app\modules\user\models\User */
+
+$this->title = Yii::t('app', 'TITLE_PROFILE');
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="user-profile">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= DetailView::widget([
+        'model' => $model,
+        'attributes' => [
+            'username',
+            'email',
+        ],
+    ]) ?>
+
+</div>
