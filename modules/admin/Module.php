041e2d3e53a4b19c485a5609845c5ef056eadca7
pamparam <pamparam83@gmail.com>
2016-12-14 22:27:51 +0300

    control access module admin

--- modules/admin/Module.php
+++ modules/admin/Module.php
@@ -2,23 +2,24 @@
 
 namespace app\modules\admin;
 
-/**
- * admin module definition class
- */
+use yii\filters\AccessControl;
+
 class Module extends \yii\base\Module
 {
-    /**
-     * @inheritdoc
-     */
     public $controllerNamespace = 'app\modules\admin\controllers';
 
-    /**
-     * @inheritdoc
-     */
-    public function init()
+    public function behaviors()
     {
-        parent::init();
-
-        // custom initialization code goes here
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+        ];
     }
 }
